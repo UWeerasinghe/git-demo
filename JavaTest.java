e91d73cc7a301098d9a9e40d39534f8d8f98ed7a
Udari Weerasinghe <weerasingheudari@gmail.com>
2025-08-16 11:32:32 +0400

    Revert "fourth commit"
    This reverts commit 7f7bec3431af1f3ef3a51b7e24a37ad5a10d364f.

--- JavaTest.java
+++ JavaTest.java
@@ -2,6 +2,5 @@ public class GitTestJava{
 	public static void main(String []args){
 		system.out.println("This is a Test");
 		system.out.println("Test");
-		system.out.println("This is a Test 5");
 	}
 }
